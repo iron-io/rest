a4926f0d58abf6f637878141058ea717c0dc1bef
Travis Reeder <treeder@gmail.com>
2012-03-04 13:19:52 -0800

    hacking on retries, not done.

--- lib/rest/client.rb
+++ lib/rest/client.rb
@@ -41,12 +41,14 @@ module Rest
 
   class Client
 
+    attr_accessor :options
     # options:
     # - :gem => specify gem explicitly
     #
     def initialize(options={})
       @logger = Logger.new(STDOUT)
       @logger.level=Logger::INFO
+      @options = options
 
       Rest.gem = options[:gem] if options[:gem]
 
@@ -58,24 +60,44 @@ module Rest
 
     end
 
-
     def get(url, req_hash={})
-      @wrapper.get(url, req_hash)
-    end
+      max_retries = @options[:retries] || 0
+      current_retry = 0
+      success = false
+      while current_retry <= max_retries do
+        #fmt.Println(num, "Pushing", i, "try", currentRetry, "total:", TotalCount)
+        res = @wrapper.get(url, req_hash)
+        if current_retry >= max_retries
+          return res
+        end
+        if res.code == 503
+          pow = (4 ** current_retry) * 100 # milliseconds
+          puts 'pow=' + pow.to_s
+          s = Random.rand * pow
+          puts 's=' + s.to_s
+          sleep_secs = 1.0 * s / 1000.0
+          puts 'sleep for ' + sleep_secs.to_s
+          sleep sleep_secs
+        else
+          success = true
+          break
+        end
+        current_retry += 1
+      end
 
-    # req_hash options:
-    # - :body => post body
-    #
-    def post(url, req_hash={})
-      @wrapper.post(url, req_hash)
-    end
+      # req_hash options:
+      # - :body => post body
+      #
+      def post(url, req_hash={})
+        @wrapper.post(url, req_hash)
+      end
 
-    def put(url, req_hash={})
-      @wrapper.put(url, req_hash)
-    end
+      def put(url, req_hash={})
+        @wrapper.put(url, req_hash)
+      end
 
-    def delete(url, req_hash={})
-      @wrapper.delete(url, req_hash)
+      def delete(url, req_hash={})
+        @wrapper.delete(url, req_hash)
+      end
     end
   end
-end
